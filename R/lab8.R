046d7cf95088470b08a98b23140763725b1afda0
Yoon-Sangwon <tkddnjs1648@gmail.com>
2021-03-04 11:21:25 +0900

    UPDATE lab8.R

--- R/lab8.R
+++ R/lab8.R
@@ -1,3 +1,5 @@
+library(rvest)
+
 daum <- read_html('https://news.daum.net/ranking/popular/', encoding = "UTF-8")
 url1 <- 'div.rank_news > ul.list_news2 > li:nth-child('
 url2 <- ") > div.cont_thumb > strong > a"
@@ -6,7 +8,7 @@ url4 <- ') > div.cont_thumb > strong > span'
 newstitle <- NULL
 newspapername <- NULL
 for (i in 1:50) {
-  content_title <-paste(url1, i, url2, sep = "")
+  content_title <-paste0(url1, i, url2)
   content_name <- paste(url3, i, url4, sep = "")
   newstitle[i] <- html_text(html_nodes(daum, content_title))
   newspapername[i] <- html_text(html_nodes(daum, content_name))
